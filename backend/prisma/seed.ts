092e4f7f93efae5da9a726023c687e4dd5837f41
yluiz <luizvictor1231@gmail.com>
2024-07-31 22:12:24 -0400

    feat: add user and products seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,21 +1,35 @@
 import { PrismaClient } from '@prisma/client';
 import * as dotenv from 'dotenv';
 import { categoriesSeed } from './seeds/categories';
+import { prodcuts } from './seeds/products';
+import { user } from './seeds/user';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const rounds = categoriesSeed.length;
   dotenv.config();
   console.log('Seeding...');
+
+  // User
+  await prisma.user.create({ data: user });
+
   // Categories
-  for (let i = 0; i < rounds; i++) {
-    await prisma.category.create({ data: categoriesSeed[i]});
+  for (let c of categoriesSeed) {
+    await prisma.category.create({ data: c });
+  }
+
+  // Products
+  for (let p of prodcuts) {
+    await prisma.product.create({
+      data: p,
+    });
   }
 }
 
 main()
-  .then(() => { console.log('Done! 🍃')})
+  .then(() => {
+    console.log('Done! 🍃');
+  })
   .catch((e) => console.error(e))
   .finally(async () => {
     await prisma.$disconnect();
---
+++ backend/prisma/seeds/user.ts
@@ -0,0 +1,5 @@
+export const user = {
+    name: 'Luiz Victor',
+    email: 'luiz@gmail.com',
+    password: '$2a$10$FhZjFQ8pkCKaBqmkzOaFf.h9fKE3HRee53CZpWLBr6VSr5PH9GSqe' // luiz@123
+}
